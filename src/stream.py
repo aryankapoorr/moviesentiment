8845ee7712107283d2d5b1314bf62dfc30b7013b
aryankapoorr <aryan.kapoor.1102@gmail.com>
2024-04-23 17:06:42 -0500

    reroute file paths

--- src/stream.py
+++ src/stream.py
@@ -19,7 +19,7 @@ def load_data(csv_file):
 
 
 # Assuming you have a CSV with movie data and poster URLs
-data = load_data('Sentiment Analysis Data - Sheet1.csv')
+data = load_data('../data/Sentiment Analysis Data - Sheet1.csv')
 
 # Load the Keras model
 
